5dcfc7180e997350de41fbb608d868ad9aeffdd3
deeptrop <I7732763@mail.ru>
2024-10-12 16:08:10 +0300

    Synchroniing

---
+++ training work with git/code.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int main() {
+    int number;
+    number = 123456890;
+    std::cout << number;
+    
+    int numbers;
+    numbers = 987;
+    std::cin >> numbers;
+
+    
+
+    return 0;
+}
